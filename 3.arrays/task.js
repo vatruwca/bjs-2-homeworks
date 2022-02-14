e809374e633ef5dfd0dc68d2273f85ab17c3473a
vatruwca <vatruwca@gmail.com>
2022-02-14 10:02:29 +0300

    решение ДЗ

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,10 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+function compareArrays (arr1, arr2) {
+  let result = arr1.length == arr2.length && arr1.every((item, i) => item === arr2[i]);  
+  return result; 
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  resultArr = arr.filter((a) => a > 0).filter((a) => a % 3 === 0).map((a) => a*10)
+  return resultArr; 
 }
