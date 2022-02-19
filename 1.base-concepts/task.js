c617efc49d162d8f0f529f48f4df417098b9c693
vatruwca <vatruwca@gmail.com>
2022-02-19 23:07:38 +0300

    решение ДЗ

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,15 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict"
+function solveEquation(a, b, c) {  
+  let arr = [];
+  const d = Math.pow(b, 2) - (4 * a * c);
+  if (a != 0) { 
+    if (d > 0) {
+      arr.push(((-b + Math.sqrt(d) ) / (2 * a)), ((-b - Math.sqrt(d) ) / (2 * a)));
+    } else if (d === 0) {
+      arr.push(-b / (2 * a) );
+    } 
+  }  
+  return arr; 
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
