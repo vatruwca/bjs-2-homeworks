0d0b2131bcfeab2c5f748a017f1dd1e60d0926bd
vatruwca <vatruwca@gmail.com>
2022-04-19 21:43:19 +0300

    first commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,48 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+  function wrapper(...args){
+    const key = args.join(','); 
+    let idx = cache.findIndex(item => item.key === key); 
+    if (idx !== -1 ) { 
+        console.log("Из кэша: " + cache[idx].result); 
+        return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args); 
+    cache.push({key, result}) ; 
+    if (cache.length > 5) { 
+      cache.shift();  
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }  
+  return wrapper
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) { 
+  let flag = false;
+  function wrapper(...args) {    
+    if (flag) return;
+    func(...args);
+    flag = true;
+    setTimeout(() => flag = false, ms);
+  }
+     return wrapper  
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+
+function debounceDecorator2(func, ms) {   
+  let flag = false;
+  let count = 0;
+  function wrapper(...args) {    
+    console.log('Функция вызвана ' + count++ + ' раз');
+     if (flag) return;
+     func(...args);
+     flag = true;
+     setTimeout(() => flag = false, ms);
+  }      
+     return wrapper 
 }
+
+
